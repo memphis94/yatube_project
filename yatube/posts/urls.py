678f1f75c0219219ddd11caf57e0a29e8feabe62
Alexey Petukhov <69753101+memphis94@users.noreply.github.com>
2022-06-21 05:19:54 +0300

    New beginning

--- yatube/posts/urls.py
+++
@@ -1,9 +0,0 @@
-# posts/urls.py
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', group_posts)
-] 
--- yatube/posts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    return render(request, template)

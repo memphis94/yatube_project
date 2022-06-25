d56a378704e94af09351e0a6af6892a05c9b0818
Alexey Petukhov <69753101+memphis94@users.noreply.github.com>
2022-06-25 19:50:38 +0300

    groups and posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post, Group
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]    
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
-    return HttpResponse(f'Посты группы {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'    
+    context = {      
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
